6061fa48a93d683cb33c3d716037e68d3b510bbe
lihaocheng <lihaocheng00@126.com>
2019-04-05 23:11:40 +0800

    init

---
+++ src/test/java/com/metastore/metastore/MetastoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.metastore.metastore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MetastoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
